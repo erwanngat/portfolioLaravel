10ccd3de34478bb4c573ec325cf10fc1b72c14ea
GATINEL Erwann <ewnran69@yahoo.fr>
2023-12-10 17:36:36 +0100

    add navbar

---
+++ resources/views/components/navBar.blade.php
@@ -0,0 +1,29 @@
+<style>
+    .dropdown:hover .dropdown-menu {
+        display: block;
+    }
+</style>
+
+<nav class="bg-gray-800 py-4">
+    <div class="flex justify-between items-center px-8">
+        <a href="#" class="text-white font-bold text-2xl">GATINEL Erwann</a>
+        <div class="flex items-center">
+            <a href="#" class="text-white font-bold mx-4">Accueil</a>
+            <a href="#" class="text-white mx-4">BTS SIO</a>
+
+            <div class="dropdown relative">
+                <a href="#" class="text-white mx-4">Parcours de professionnalisation ↓</a>
+                <ul class="dropdown-menu absolute hidden bg-gray-800 py-2">
+                    <li><a href="#" class="text-white block px-4 py-2 hover:bg-gray-700 w-64">Parcours
+                            professionnel</a></li>
+                    <li><a href="#" class="text-white block px-4 py-2 hover:bg-gray-700">Projets</a></li>
+                    <li><a href="#" class="text-white block px-4 py-2 hover:bg-gray-700">Portefeuille de
+                            compétences</a></li>
+                </ul>
+            </div>
+            <a href="#" class="text-white mx-4">Veille technologique</a>
+            <a href="#" class="text-white mx-4">Patrimoine informatique</a>
+            <a href="#" class="text-white mx-4">Contact</a>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-navBar/>
+    <p class='text-4xl'>home</p>
+</x-layout>
