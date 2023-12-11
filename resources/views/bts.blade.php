002cdd13d8c61ff3572b740e941ec76f94c18e04
GATINEL Erwann <ewnran69@yahoo.fr>
2023-12-11 10:03:09 +0100

    css liste matiere

--- resources/views/bts.blade.php
+++ resources/views/bts.blade.php
@@ -2,63 +2,77 @@
     <x-slot name='header'>
         {{ "Qu'est ce que le BTS SIO ?" }}
     </x-slot>
-    <div class='bg-gray-200'>
-        <div class='flex flex-row py-8 justify-center'>
-            <div class='w-1/3'>
-                <p class='text-xl text-justify pt-8'>En deux ans, les étudiants du BTS Service Informatique aux
-                    Organisations (SIO) sont formés à de nombreux métiers
-                    comme ceux de développeur ou encore d’administrateur. Ce programme offre une compréhension
-                    approfondie des systèmes informatiques, couvrant à la fois la conception logicielle et les aspects
-                    techniques de la gestion des réseaux.</p>
+    <div class='bg-gay-200'>
+        <div class='bg-gray-200'>
+            <div class='flex flex-row py-8 justify-center'>
+                <div class='w-1/3'>
+                    <p class='text-xl text-justify pt-8'>En deux ans, les étudiants du BTS Service Informatique aux
+                        Organisations (SIO) sont formés à de nombreux métiers
+                        comme ceux de développeur ou encore d’administrateur. Ce programme offre une compréhension
+                        approfondie des systèmes informatiques, couvrant à la fois la conception logicielle et les
+                        aspects
+                        techniques de la gestion des réseaux.</p>
+                </div>
+                <div class='w-1/4 ml-48 pt-8'>
+                    <img src='images/school.png' class='flex'>
+                </div>
             </div>
-            <div class='w-1/4 ml-48 pt-8'>
-                <img src='images/school.png' class='flex'>
+
+            <div class='flex flex-row py-8 justify-center'>
+                <div class='mr-48 pt-8'>
+                    <img src='images/logoSlam.png' class='flex'>
+                </div>
+                <div class='w-1/3'>
+                    <p class='text-xl text-justify pt-8'>Le BTS SIO option SLAM (Solutions Logicielles et Applications
+                        Métiers) est une option qui vise à former les étudiants à concevoir et à mettre en œuvre des
+                        applications informatiques destinées à répondre aux exigences et aux contraintes des divers
+                        secteurs
+                        d'activité professionnelle. Elle peut englober des compétences en développement logiciel, en
+                        bases
+                        de données, en analyse de besoins métiers, voire en conception d'interfaces utilisateurs.</p>
+                </div>
+            </div>
+
+            <div class='pl-16 pb-8'>
+                <h1 class='text-4xl'>Les matières enseignées et épreuves :</h1>
+
+                <div class="flex">
+                    <div class="w-1/2">
+                        <ul class="text-3xl pt-4">
+                            <p class="font-bold py-4">Matières enseignées :</p>
+                        </ul>
+                        <li class='text-lg py-2'>Culture générale et expression</li>
+                        <li class='text-lg py-2'>Expression et communication en langue anglaise</li>
+                        <li class='text-lg py-2'>Mathématiques pour l’informatique</li>
+                        <li class='text-lg py-2'>Algorithmique appliquée</li>
+                        <li class='text-lg py-2'>Analyse juridique des services informatiques</li>
+                        <li class='text-lg py-2'>Analyse économique, managériale des services informatiques</li>
+                        <li class='text-lg py-2'>Support système des accès utilisateurs</li>
+                        <li class='text-lg py-2'>Support réseau des accès utilisateurs</li>
+                        <li class='text-lg py-2'>Exploitation des données</li>
+                        <li class='text-lg py-2'>Bases de la programmation</li>
+                        <li class='text-lg py-2'>Développement d’application</li>
+                        <li class='text-lg py-2'>Intégration et adaptation d’un service</li>
+                    </div>
+
+                    <div class="w-1/2 pl-8">
+                        <ul class="text-3xl pt-4">
+                            <p class="font-bold py-4">Épreuves :</p>
+                        </ul>
+                        <li class='text-lg py-2'>Culture générale et expression</li>
+                        <li class='text-lg py-2'>Expression et communication en langue anglaise</li>
+                        <li class='text-lg py-2'>Mathématiques pour l’informatique</li>
+                        <li class='text-lg py-2'>Mathématiques pour l’informatique (partie algo)</li>
+                        <li class='text-lg py-2'>Culture économique, managériale et juridique pour l’informatique</li>
+                        <li class='text-lg py-2'>E4 Support et mise à disposition des services informatiques</li>
+                        <li class='text-lg py-2'>E5 Conception et développement d’applications (SLAM)</li>
+                        <li class='text-lg py-2'>E6 Cybersécurité des services informatiques (SISR/SLAM)</li>
+                        <li class='text-lg py-2'>Mathématiques approfondies</li>
+                        <li class='text-lg py-2'>Parcours de certification complémentaire </li>
+                    </div>
+                </div>
             </div>
         </div>
-
-        <div class='flex flex-row py-8 justify-center'>
-            <div class='mr-48 pt-8'>
-                <img src='images/logoSlam.png' class='flex'>
-            </div>
-            <div class='w-1/3'>
-                <p class='text-xl text-justify pt-8'>Le BTS SIO option SLAM (Solutions Logicielles et Applications
-                    Métiers) est une option qui vise à former les étudiants à concevoir et à mettre en œuvre des
-                    applications informatiques destinées à répondre aux exigences et aux contraintes des divers secteurs
-                    d'activité professionnelle. Elle peut englober des compétences en développement logiciel, en bases
-                    de données, en analyse de besoins métiers, voire en conception d'interfaces utilisateurs.</p>
-            </div>
-        </div>
-
-        <p>Les matières enseignées</p>
-
-        <ul class="text-xl pt-4">
-            <p class="font-bold">Matières enseignées :</p>
-            <li>Culture générale et expression</li>
-            <li>Expression et communication en langue anglaise</li>
-            <li>Mathématiques pour l’informatique</li>
-            <li>Algorithmique appliquée</li>
-            <li>Analyse juridique des services informatiques</li>
-            <li>Analyse économique, managériale des services informatiques</li>
-            <li>Support système des accès utilisateurs</li>
-            <li>Support réseau des accès utilisateurs</li>
-            <li>Exploitation des données</li>
-            <li>Bases de la programmation</li>
-            <li>Développement d’application</li>
-            <li>Intégration et adaptation d’un service</li>
-        </ul>
-
-        <ul class="text-xl pt-4">
-            <p class="font-bold">Épreuves :</p>
-            <li>Culture générale et expression</li>
-            <li>Expression et communication en langue anglaise</li>
-            <li>Mathématiques pour l’informatique</li>
-            <li>Mathématiques pour l’informatique (partie algo)</li>
-            <li>Culture économique, managériale et juridique pour l’informatique</li>
-            <li>E4 Support et mise à disposition des services informatiques</li>
-            <li>E5 Conception et développement d’applications (SLAM)</li>
-            <li>E6 Cybersécurité des services informatiques (SISR/SLAM)</li>
-            <li>Mathématiques approfondies</li>
-            <li>Parcours de certification complémentaire </li>
-        </ul>
+    </div>
 
 </x-layout>
